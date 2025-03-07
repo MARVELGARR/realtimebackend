2b7989af58ef8d189c58cc1dd7e94b538592beb2
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-03-07 12:55:39 +0100

    Delete MainApp directory

--- MainApp/configs/cors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const corsOptions = {
-    origin: `${process.env.FRONTEND_URL}`, // Your frontend URL
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-};
-exports.default = corsOptions;
