fe9b609e1a55639896c8cf9c04893db40c0044e5
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-03-11 10:15:18 +0100

    add grouping option to getAndFilterChats action for enhanced chat retrieval

--- app/actions/userManagementActions/getAndFilterChats.ts
+++ app/actions/userManagementActions/getAndFilterChats.ts
@@ -45,6 +45,7 @@ export const getAndFilterChats: RequestHandler = async (
                   },
                 },
               },
+              group: true
             },
           },
           messages: {
