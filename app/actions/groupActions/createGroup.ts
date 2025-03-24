8785859a0861b90185683a663d790d62e9e10e68
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-03-24 11:11:42 +0100

    feat: update createGroup action to use single groupImage and rename description to groupDescription

--- app/actions/groupActions/createGroup.ts
+++ app/actions/groupActions/createGroup.ts
@@ -1,16 +1,17 @@
 import { RequestHandler, Response, Request } from "express";
 import { prisma } from "../../configs/prisma";
+import { $Enums, GroupRole, Prisma } from "@prisma/client";
 
 
 type disappearingMessages = "OFF" | "DAYS90" |'DAYS7' |"H24"
 
 
 export type newGroupDetailsProp = {
-    name: string
-    participant: string[],
-    groupImage: string[] 
-    disappearingMessages: disappearingMessages
-    description: string
+  name: string
+  participant: string[],
+  groupImage: string, 
+  disappearingMessages: disappearingMessages
+  groupDescription: string
     
 }
 
@@ -24,6 +25,9 @@ const createGroup: RequestHandler = async( req: Request, res: Response) => {
 
     const {newGroupDetails}: {newGroupDetails:newGroupDetailsProp} = req.body 
 
+    const adminId = user.userId
+    const others = newGroupDetails.participant.filter((Ids) => Ids !== adminId)
+
     if(!newGroupDetails){
         res.status(400).json({error: "no group details found"})
         return
@@ -36,15 +40,32 @@ const createGroup: RequestHandler = async( req: Request, res: Response) => {
              return
         }
 
-        if (!newGroupDetails.groupImage || newGroupDetails.groupImage.length === 0) {
-             res.status(400).json({ error: "Group image is required" });
-             return
-        }
 
         if(!newGroupDetails.name){
             res.status(400).json({ error: "Group name is required" });
             return
         }
+
+        type GroupWithConversation = Prisma.GroupGetPayload<{
+          name: string;
+          id: string;
+          createdAt: Date;
+          updatedAt: Date;
+          groupImage: string | null;
+          disappearingMessages: $Enums.DisappearingMessages;
+          descriptions: string | null;
+          creatorId: string;
+          adminId: string;
+          include: { 
+            Conversation: { 
+              select: { 
+                id: true;
+                participants: true;
+              } 
+            } 
+          }
+        }>;
+
         const result = await prisma.$transaction(async (tx) => {
           const newGroup = await tx.group.create({
             data: {
@@ -58,23 +79,27 @@ const createGroup: RequestHandler = async( req: Request, res: Response) => {
               Conversation: {
                 create: {
                   participants: {
-                    create: [...newGroupDetails.participant.map((id) => ({ userId: id }))]
+                    create: [ { userId: user.userId, groupRole: GroupRole.ADMIN },
+                      ...others.map((id) => ({ 
+                        userId: id,
+                        groupRole: GroupRole.PARTICIPANT
+                      }))]
                   }
                 }
               },
-              descriptions: newGroupDetails.description,
-              disappearingMessages: newGroupDetails.disappearingMessages,
-              groupImage: newGroupDetails.groupImage[0]
+              descriptions: newGroupDetails.groupDescription,
+              disappearingMessages: newGroupDetails.disappearingMessages || "OFF",
+              groupImage: newGroupDetails.groupImage
             },
-            include: {
+            include:{
               Conversation: {
-                select: {
+                select:{
                   id: true,
                   participants: true
                 }
               }
             }
-          });
+          }) as GroupWithConversation;
         
           const addParticipant = await tx.group.update({
             where: {
@@ -88,8 +113,9 @@ const createGroup: RequestHandler = async( req: Request, res: Response) => {
               },
             }
           });
-        
-          return { newGroup, addParticipant };
+          
+
+          return { newGroup, addParticipant,  };
         });
         
         
