fcd8bd0df6aee81d91f2e29f9de23cedfddd977b
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-07-26 20:35:52 +0100

    wdwd

--- app/actions/authActions/google.ts
+++ app/actions/authActions/google.ts
@@ -137,7 +137,7 @@ export const googleCallback = async (
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
       });
 
-     res.status(200)
+     
      return
       
     } else {
