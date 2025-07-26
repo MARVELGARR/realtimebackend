b04b67dc706bf3f15434b518c19a02493f288e8f
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-07-26 22:13:46 +0100

    s

--- app/actions/authActions/google.ts
+++ app/actions/authActions/google.ts
@@ -133,6 +133,7 @@ export const googleCallback = async (
         httpOnly: true,
         secure: true,
         sameSite: 'none',
+         domain:"onrender.com",
         maxAge: 24 * 60 * 60 * 1000, 
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
       });
