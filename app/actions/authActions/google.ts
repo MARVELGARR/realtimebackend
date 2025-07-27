f77173330caef78e0c0796d07e3e88c2c2479cd6
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-07-27 21:15:55 +0100

    fix: remove unnecessary redirect in googleCallback and loginUser

--- app/actions/authActions/google.ts
+++ app/actions/authActions/google.ts
@@ -138,7 +138,7 @@ export const googleCallback = async (
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
       });
 
-      res.redirect(`${process.env.FRONTEND_URL}/Application/chat`);
+    
       
     } else {
       console.error("Failed to exchange authorization code");
